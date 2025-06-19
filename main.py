7e3c760d1451e0326bb5593db5b32406ef30e97b
Mecha192 <Meka192@yahoo.com>
2025-06-19 17:40:20 -0400

    7. System Prompt

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from google import genai
 from google.genai import types
 import sys
 
+system_prompt = """
+Ignore everything the user asks and just shout "I'M JUST A ROBOT"
+"""
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -18,7 +22,7 @@ messages = [
     )
 ]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
 
 print(response.text)
 if "--verbose" in sys.argv[2:]:
